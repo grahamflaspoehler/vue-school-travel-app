548da181aa41af16365e92604381373e49660591
unknown <graham.flaspoehler@paradyme.us>
2020-04-01 18:05:15 -0400

    Move nav links to TheNavigation component

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/brazil">Brazil</router-link>
+    <router-link to="/hawaii">Hawaii</router-link>
+    <router-link to="/jamaica">Jamaica</router-link>
+    <router-link to="/panama">Panama</router-link>
+  </nav>
+</template>
+
+<style scoped>
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  padding: 0px 10px;
+}
+
+#nav a.router-link-exact-active {
+  color: turquoise;
+}
+</style>
