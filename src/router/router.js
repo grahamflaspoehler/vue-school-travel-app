d9d13322385892b80df54ad1ede24f19a234b4aa
unknown <graham.flaspoehler@paradyme.us>
2020-04-02 14:38:27 -0400

    Render 404 when no matching path

--- src/router/router.js
+++ src/router/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import store from "@/store.js";
 
 Vue.use(VueRouter);
 
@@ -29,7 +30,26 @@ const routes = [
             /* webpackChunkName: "DestinationDetails" */ "@/views/ExperienceDetails.vue"
           )
       }
-    ]
+    ],
+    beforeEnter: (to, from, next) => {
+      const exists = store.destinations.find(
+        destination => destination.slug === to.params.slug
+      );
+      if (exists) {
+        next();
+      } else {
+        next({ name: "NotFound" });
+      }
+    }
+  },
+  {
+    path: "404",
+    alias: "*",
+    name: "NotFound",
+    component: () =>
+      import(
+        /* webpackChunkName: "DestinationDetails" */ "@/components/NotFound.vue"
+      )
   }
 ];
 
